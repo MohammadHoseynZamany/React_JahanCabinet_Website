b6f6033ddcab3bfe81cc6c925f0d604021744a44
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2022-11-30 14:08:11 +0330

    create project's pages

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import Samples from "./pages/Samples"
+
+export default function App(){
+  return (
+    <div>
+      <Home />
+      <About />
+      <Contact />
+      <Samples />
+    </div>
+  )
+}
