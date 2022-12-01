4639a90141277a366ace3b2313065c580a5e1539
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2022-12-01 13:58:27 +0330

    add SeeDetails page

--- src/components/KitchentCabinet.jsx
+++ src/components/KitchentCabinet.jsx
@@ -5,16 +5,17 @@ import image from `../images/cvb.jpg`
 export default function KitchenCabinet(props){
     function handleClick(){
         return(
-            <SeeDetails url={props.url} title={props.title} details={props.details} />
+            // <SeeDetails url={props.url} title={props.title} details={props.details} />
+            <SeeDetails url={image} title={props.title} details={props.details} />
         )
     }
     const Image = `../images/cvb.jpg`
     
     return(
         <div className="KitchenCabinet-container" onClick={handleClick}>
-            {/* <img src={`${props.url}`} alt={props.title} /> */}
-            <img src={image} alt={props.title} />
-            <p>{props.title}</p>
+            {/* <img src={image} alt={props.title} />
+            <p>{props.title}</p> */}
+            <SeeDetails url={image} title={props.title} details={props.details} />
         </div>
     )
 }
--- src/components/SeeDetails.jsx
+++ src/components/SeeDetails.jsx
@@ -1,11 +1,26 @@
 import React from "react"
+import {
+    MDBCard,
+    MDBCardBody,
+    MDBCardTitle,
+    MDBCardText,
+    MDBCardImage,
+    MDBRipple
+} from 'mdb-react-ui-kit';
 
 export default function SeeDetails(props){
     return (
-        <div className="SeeDetails-container">
-            <img src={props.url} alt={props.title} />
-            <h2>{props.title}</h2>
-            <p>{props.details}</p>
-        </div>
+        <MDBCard>
+            <MDBRipple rippleColor='light' rippleTag='div' className='bg-image hover-overlay'>
+                <MDBCardImage src={props.url} position="top" alt={props.details} />
+                <a>
+                    <div className='mask' style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+                </a>
+            </MDBRipple>
+            <MDBCardBody>
+                <MDBCardTitle>{props.title}</MDBCardTitle>
+                <MDBCardText>{props.details}</MDBCardText>
+            </MDBCardBody>
+        </MDBCard>
     )
 }
