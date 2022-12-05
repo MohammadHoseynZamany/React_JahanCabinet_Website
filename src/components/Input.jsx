ad9e95780a402da82fea89bb2e5f79a6705a95ad
MohammadHoseynZamany <mohammadhoseynzamany@gmail.com>
2022-12-05 08:18:52 +0330

    add input titles and descriptions

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,112 +3,112 @@ export default [
         id: 1,
         type: "KitchenCabinet",
         url: "https://www.liveenhanced.com/wp-content/uploads/2021/04/Kitchen-Cabinets-3-1024x564.jpg",
-        title: "image title",
-        details: "image details"
+        title: "ممبران طوسی",
+        details: "کابینت ممبران طوسی رنگ  با کد رنگ 3312 به همراه جزیره و صفحه کورین"
     },
     {
         id: 2,
         type: "KitchenCabinet",
         url: "https://www.liveenhanced.com/wp-content/uploads/2021/04/Kitchen-Cabinets-1-1024x683.jpg",
-        title: "image title",
-        details: "image details"
+        title: "ممبران قهوه ایی",
+        details: "کابینت ممبران قهوه ایی با کد رنگ 7621 و صفحه سنگ"
     },
     {
         id: 3,
         type: "KitchenCabinet",
         url: "https://www.liveenhanced.com/wp-content/uploads/2021/04/Kitchen-Cabinets-2-1024x693.jpg",
-        title: "image title",
-        details: "image details"
+        title: "طرح چوب",
+        details: "کابینت طرح چوب با کد رنگ 1234 به همراه جزیره"
     },
     {
         id: 4,
         type: "KitchenCabinet",
         url: "https://cdn.vox-cdn.com/thumbor/1L4a4ve76ZqVZyjn0h5FWWxbcmQ=/0x0:4000x2667/920x613/filters:focal(1680x1014:2320x1654):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/65889507/0120_Westerly_Reveal_6C_Kitchen_Alt_Angles_Lights_on_15.14.jpg",
-        title: "image title",
-        details: "image details"
+        title: "ممبران سفید",
+        details: "کابینت ممبران سفید با کد رنگ 1142 و آبی با کد رنگ 1345 به همراه جزیره"
     },
     {
         id: 5,
         type: "Closet",
         url: "https://khabrdagh.ir/wp-content/uploads/2021/11/%DA%A9%D9%85%D8%AF-%D9%84%D8%A8%D8%A7%D8%B3-%DA%A9%D8%AF-FH113.jpg",
-        title: "image title",
-        details: "image details"
+        title: "ملامین سفید",
+        details: "کمد لباسی با ملامین سفید به کد رنگ 1121"
     },
     {
         id: 6,
         type: "Closet",
         url: "https://khabrdagh.ir/wp-content/uploads/2021/11/%DA%A9%D9%85%D8%AF-%DA%86%D9%86%D8%AF%D9%85%D9%86%D8%B8%D9%88%D8%B1%D9%87-%D9%85%D8%AF%D9%84-FH167.jpg",
-        title: "image title",
-        details: "image details"
+        title: "ام دی اف شیری",
+        details: "کمد لباسی با ام دی اف شیری به کد رنگ 1773"
     },
     {
         id: 7,
         type: "Closet",
         url: "https://khabrdagh.ir/wp-content/uploads/2021/11/%DA%A9%D9%85%D8%AF-%D9%84%D8%A8%D8%A7%D8%B3-%D9%85%D8%AF%D9%84-FH262.jpg",
-        title: "image title",
-        details: "image details"
+        title: "ملامین قهوه ایی سوخته",
+        details: "کمد لباسی با ملامین به رنگ قهوه ایی سوخته با کد رنگ 1144"
     },
     {
         id: 8,
         type: "Closet",
         url: "https://newkhane.ir/wp-content/uploads/2021/07/storage-dresser-768x591.jpg",
-        title: "image title",
-        details: "image details"
+        title: "ممبران طوسی",
+        details: "کمد لباسی با ملامین به رنگ طوسی با کد رنگ 5698"
     },
     {
         id: 9,
         type: "ShowCase",
         url: "https://salamsakhteman.com/blog/wp-content/uploads/2020/06/image13-9.jpeg",
-        title: "image title",
-        details: "image details"
+        title: "ممبران سفید",
+        details: "ویترین و کابینت با ممبران سفید با کد رنگ 5674"
     },
     {
         id: 10,
         type: "ShowCase",
         url: "https://salamsakhteman.com/blog/wp-content/uploads/2020/06/image1-7.jpeg",
-        title: "image title",
-        details: "image details"
+        title: " ام دی اف شیری ",
+        details: "ویترین و کابینت با ام دی اف شیری به کد رنگ 2256"
     },
     {
         id: 11,
         type: "ShowCase",
         url: "https://salamsakhteman.com/blog/wp-content/uploads/2020/06/image2-6.jpeg",
-        title: "image title",
-        details: "image details"
+        title: "ممبران سفید",
+        details: "ویترین و کابینت با ممبران سفید به کد رنگ 3456 و صفحه کورین"
     },
     {
         id: 12,
         type: "ShowCase",
         url: "https://salamsakhteman.com/blog/wp-content/uploads/2020/06/image3-8.jpeg",
-        title: "image title",
-        details: "image details"
+        title: "ممبران سفید",
+        details: "ویترین با ممبران سفید به کد رنگ 4467 و صفحه سنگ"
     },
     {
         id: 13,
         type: "TvDesk",
         url: "https://cabinet-karchoob.ir/content/upload/Post/50144540ed604862935b1a131526ffc5.jpg",
-        title: "image title",
-        details: "image details"
+        title: "ام دی اف سفید",
+        details: "میز تلویزیون با ام دی اف سفید به کد رنگ 1123"
     },
     {
         id: 14,
         type: "TvDesk",
         url: "https://cabinet-karchoob.ir/Content/Upload/Editor/15/%D9%85%DB%8C%D8%B2%20%D8%A7%D9%85%20%D8%AF%DB%8C%20%D8%A7%D9%81.jpg",
-        title: "image title",
-        details: "image details"
+        title: "ام دی اف سفید و قهوه ایی ",
+        details: "میز تلویزیون با ام دی اف سفید به کد رنگ 1123 و قهوه ایی به کد رنگ 4463"
     },
     {
         id: 15,
         type: "TvDesk",
         url: "https://cabinet-karchoob.ir/Content/Upload/Editor/15/%D9%85%DB%8C%D8%B2-%D8%AA%D9%84%D9%88%DB%8C%D8%B2%DB%8C%D9%88%D9%86-%D8%B4%DB%8C%DA%A9.jpg",
-        title: "image title",
-        details: "image details"
+        title: "نئوپان سفید و قهوه ایی با طرح گل",
+        details: "میز تلویزیون با نئوپان سفید به کد رنگ 1123و قهوه ایی به کد رنگ 2345"
     },
     {
         id: 16,
         type: "TvDesk",
         url: "https://cabinet-karchoob.ir/Content/Upload/Editor/15/%D9%85%DB%8C%D8%B2-%D8%A7%D9%85%20%D8%AF%DB%8C%20%D8%A7%D9%81-%D8%A7%D9%84%20%D8%B3%DB%8C%20%D8%AF%DB%8C.jpg",
-        title: "image title",
-        details: "image details"
+        title: "هایگلاس سفید و قهوه ایی سوخته",
+        details: "میز تلویزیون با هایگلاس سفید به کد رنگ 3345 و قهوه ایی تیره به کد رنگ 6789"
     }
 ]
